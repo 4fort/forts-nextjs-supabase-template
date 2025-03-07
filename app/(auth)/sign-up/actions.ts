43ee77591fc2e1bc4dff950c46b414679583c8e0
4Fort <dexterfortbsilva@gmail.com>
2025-03-07 16:26:00 +0800

    Refactor login and sign-up form submission to be asynchronous and add loading indicator

--- app/(auth)/sign-up/actions.ts
+++ app/(auth)/sign-up/actions.ts
@@ -1,7 +1,7 @@
 import { signUp } from "@/lib/supabase/supabase-auth";
 import { SignUpFormValuesType } from "./_lib/signUpFormSchema";
 
-export default function handleSubmit(values: SignUpFormValuesType) {
+export default async function handleSubmit(values: SignUpFormValuesType) {
   const formData = new FormData();
   formData.append("email", values.email);
   formData.append("password", values.password);
